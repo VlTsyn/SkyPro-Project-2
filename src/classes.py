07940fbcc2e20e2cab75368de252b4ce3e80225f
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2025-08-05 00:19:15 +0300

    add private atributes

--- src/classes.py
+++ src/classes.py
@@ -1,27 +1,56 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, product):
+        name = product["name"]
+        description = product["description"]
+        price = product["price"]
+        quantity = product["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         self.product_count = len(products)
         Category.category_count +=1
         Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.product_count += 1
+        return self.__products.append(product)
+
+    @property
+    def products(self):
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n")
+        return result
