ca0d80cb90c353a1bf7b75a098c55adf8395e03f
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2025-08-17 23:19:15 +0300

    add subclasses fixes

--- src/classes.py
+++ src/classes.py
@@ -14,7 +14,7 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if isinstance(other, self.__class__):
+        if type(other) is type(self):
             result = self.quantity * self.price + other.quantity * other.price
             return result
 
